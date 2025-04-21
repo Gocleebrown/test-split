6ef3f41703c71f93d8ac1f44e4a7fc0c763a69f5
Gocleebrown <guy.brown@hc.school.nz>
2025-04-22 08:50:14 +1200

    Update core.js

--- scripts/core.js
+++ scripts/core.js
@@ -1,1 +1,24 @@
-//core function
+// scripts/core.js
+
+// Global variables
+let answeredCurrent = false,
+    currentQuestion = null,
+    currentAnswers = null,
+    score = 0,
+    mixedCount = 0,
+    questionPool = [];
+
+// Utility functions
+function ordinalSuffix(n) { /* … as before … */ }
+function getRandom(arr)   { /* … as before … */ }
+
+// Chart‐drawing functions
+function drawSpringChart(canvasId, k, L0) { /* … */ }
+function drawOscilloscopeChart(canvasId, cycles, timeBase) { /* … */ }
+
+// Checkbox toggler
+function toggleByGroup(typeSuffix, isChecked) {
+  document
+    .querySelectorAll(`input[id$="-${typeSuffix}"]`)
+    .forEach(box => box.checked = isChecked);
+}
